fb4a29e9a02bd2233dfdb2548518d64d42f006ff
DupelaWebsite <dupelawebsite@gmail.com>
2025-10-17 12:32:01 +0000

    init base theme

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,27 @@
 
+const menuIcon = document.getElementById("menuIcon");
+const sideInfo = document.getElementById("sideInfo");
+
+menuIcon.addEventListener("click", () => {
+  menuIcon.classList.toggle("active");
+  sideInfo.classList.toggle("active");
+});
+
+
+const navSide = document.getElementById("navSide");
+const sideMenu = document.getElementById("sideMenu");
+
+navSide.addEventListener("click", () => {
+  navSide.classList.toggle("active");
+  sideMenu.classList.toggle("active");
+});
+
+const submenuToggles = document.querySelectorAll(".submenu-toggle");
+
+submenuToggles.forEach(toggle => {
+  toggle.addEventListener("click", () => {
+    toggle.parentElement.classList.toggle("active");
+    const symbol = toggle.querySelector("span");
+    symbol.textContent = toggle.parentElement.classList.contains("active") ? "−" : "+";
+  });
+});
